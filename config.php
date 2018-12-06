4c4520ced143c8ce56d016f9bd983e30242d49b5
Victor Mann <victorcmann@gmail.com>
2018-12-06 21:53:19 -0200

    estrutura base para o projeto

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+require 'environment.php';
+
+global $config;
+global $db;
+
+$config = array();
+
+if (ENVIRONMENT == 'development')
+{
+	$config['dbname'] = DBNAME;
+	$config['host']   = HOST;
+	$config['dbuser'] = DBUSER;
+	$config['dbpass'] = DBPASS;
+} 
+else 
+{
+	$config['dbname'] = DBNAME;
+	$config['host']   = HOST;
+	$config['dbuser'] = DBUSER;
+	$config['dbpass'] = DBPASS;
+}
+
+$db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
---
+++ environment.example.php
@@ -0,0 +1,11 @@
+<?php
+define("ENVIRONMENT", "development");
+
+// URI
+define("BASE_URL", "http://localhost/...");
+
+// DATABASE
+define('DBNAME', 'database_name');
+define('HOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
