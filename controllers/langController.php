8b406e26a359051e8bc5da44cb1f143bf9d68a0c
Victor Mann <victorcmann@gmail.com>
2018-12-06 23:26:09 -0200

    criando controlador de alteracao da linguagem

---
+++ controllers/langController.php
@@ -0,0 +1,16 @@
+<?php
+class langController extends controller
+{
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {}
+
+    public function set($lang)
+    {
+        $_SESSION['lang'] = $lang;
+        header('Location: '. BASE_URL);
+        exit;
+    }
+}
