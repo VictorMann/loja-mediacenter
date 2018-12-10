f5919d6040c14e91df2c8eb1cdb0509608f7ae71
Victor Mann <victorcmann@gmail.com>
2018-12-10 17:29:19 -0200

    view de categorias com listagem e paginacao

---
+++ views/categories.php
@@ -0,0 +1,42 @@
+<h1><?=$catName?></h1>
+<?
+/**
+ *  Lista de produtos com grid bootstrap 
+ *  a cada trêz col-sm-4 gera uma nova linha
+ *  para receber trêz novamente....
+ */
+?>
+
+<?php $count = 0?>
+
+<div class="row">
+    <?php foreach($list as $p): ?>
+        
+        <div class="col-sm-4">
+            <?=$this->loadView('product_item', $p)?>
+        </div>
+
+        <?php $count ++?>
+
+        <?php if ($count > 2): ?>
+            </div>
+            <div class="row">
+            <?php $count = 0?>
+        <?php endif ?>
+
+    <?php endforeach ?>
+
+    <?php if ($numberOfPages > 1):?>
+        <ul class="paginate list-unstyled">
+            <?php for ($i = 1; $i <= $numberOfPages; $i ++):?>
+                <li>
+                    <a 
+                     href="?p=<?=$i?>" 
+                     class="<?=$currentPage==$i?'activated':''?>">
+                        <?=$i?>
+                    </a>
+                </li>
+            <?php endfor?>
+        </ul>
+    <?php endif?>
+</div>
