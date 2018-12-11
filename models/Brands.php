f755ea8aa77e6ce9e5458ab2a22a4bdbee791226
Victor Mann <victorcmann@gmail.com>
2018-12-11 16:27:49 -0200

    definindo metodos de busca em Brands

--- models/Brands.php
+++ models/Brands.php
@@ -1,1 +1,23 @@
 <?php
+
+class Brands extends model
+{
+    const TABLENAME = 'brands';
+
+    public function getList()
+    {
+        $sql = 'SELECT * FROM '. self::TABLENAME;
+        $sql = $this->db->query($sql);
+        return $sql->rowCount() ? $sql->fetchAll() : [];
+    }
+
+    public function getListTotalItems()
+    {
+        $sql  = 'SELECT b.id, b.name, COUNT(*) qtd FROM '. self::TABLENAME .' b ';
+        $sql .= 'INNER JOIN products p ON b.id = p.id_brand ';
+        $sql .= 'GROUP BY b.id';
+
+        $sql = $this->db->query($sql);
+        return $sql->rowCount() ? $sql->fetchAll() : [];
+    }
+}
