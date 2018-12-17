18a4d45809547705ac9754dd663946d6142eade2
Victor Mann <victorcmann@gmail.com>
2018-12-17 17:30:19 -0200

    criando buscaController e view busca

---
+++ controllers/buscaController.php
@@ -0,0 +1,56 @@
+<?php
+class buscaController extends controller {
+
+	private $user;
+
+    public function __construct() 
+    {
+        parent::__construct();
+    }
+
+    public function index() 
+    {
+        $searchTerm = !empty($_GET['s'])? $_GET['s'] : null;
+        if (!$searchTerm) {
+            header('Location: ./');
+            exit;
+        }
+
+        $dados = array();
+
+        $limit = 3;
+        $offset = 0;
+
+        $currentPage = 1;
+        if (!empty($_GET['p']) && $_GET['p'] > 0) $currentPage = (int) $_GET['p'];
+
+        $offset = $currentPage * $limit - $limit;
+
+        $products = new Products;
+        $categories = new Categories;
+        $f = new Filters;
+
+        $filters = [];
+
+        if (!empty($_GET['filter']) and is_array($_GET['filter']))
+        {
+            $filters = $_GET['filter'];
+        }
+
+        // busca
+        $filters['searchTerm'] = $searchTerm;
+        $filters['category'] = $_GET['category'];
+
+        $dados['list'] = $products->getList($offset, $limit, $filters);
+        $dados['totalItems'] = $products->getTotal($filters);
+        $dados['numberOfPages'] = ceil($dados['totalItems'] / $limit);
+        $dados['currentPage'] = $currentPage;
+        $dados['categorias'] = $categories->getList();
+        $dados['filters'] = $f->getFilters($filters);
+        $dados['filters_selected'] = $filters;
+        $dados['searchTerm'] = $searchTerm;
+        $dados['category'] = $filters['category'];
+
+        $this->loadTemplate('busca', $dados);
+    }
+}
---
+++ views/busca.php
@@ -0,0 +1,42 @@
+<?
+/**
+ *  Lista de produtos com grid bootstrap 
+ *  a cada trêz col-sm-4 gera uma nova linha
+ *  para receber trêz novamente....
+ */
+?>
+
+<?php $count = 0?>
+
+<h2>Você está procurando por: '<?=$searchTerm?>'</h2>
+<div class="row">
+    <?php foreach($list as $p): ?>
+        
+        <div class="col-sm-4">
+            <?=$this->loadView('product_item', $p)?>
+        </div>
+
+        <?php $count ++?>
+
+        <?php if ($count > 2): ?>
+            </div>
+            <div class="row">
+            <?php $count = 0?>
+        <?php endif ?>
+
+    <?php endforeach ?>
+
+    <?php if ($numberOfPages > 1):?>
+        <ul class="paginate list-unstyled">
+            <?php for ($i = 1; $i <= $numberOfPages; $i ++):?>
+                <li>
+                    <a 
+                     href="?<?=http_build_query(array_merge($_GET, ['p' => $i]))?>" 
+                     class="<?=$currentPage==$i?'activated':''?>">
+                        <?=$i?>
+                    </a>
+                </li>
+            <?php endfor?>
+        </ul>
+    <?php endif?>
+</div>
