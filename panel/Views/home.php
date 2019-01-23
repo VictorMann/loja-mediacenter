365f383cca4624051c5e6df04e5ab49837ca83e7
Victor Mann <victorcmann@gmail.com>
2019-01-23 17:06:59 -0200

    copy/paste arq lte para esqueleto do painel

--- panel/Views/home.php
+++ panel/Views/home.php
@@ -1,1 +1,18 @@
-Hello World
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        Page Header
+        <small>Optional description</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section class="content container-fluid">
+
+    Hello, World!
+
+</section>
